52edbfeb9d1065340571abfb34e42035284d0143
Jamie York <ziadoz@gmail.com>
2024-04-19 16:31:25 +0100

    simpler linked list

--- ch1/linked-list.php
+++ ch1/linked-list.php
@@ -1,29 +1,23 @@
 <?php
 class LinkedList
 {
-	public function __construct(public mixed $value, public ?LinkedList $next)
+	public function __construct(public mixed $value, public ?LinkedList $next = null)
 	{
-	}
-
-	// @todo: Implement head and tail methods. Need to hold $previous and $next for that to work.
-}
-
-class LinkedListBuilder
-{
-	public static function build(array $values): LinkedList
-	{
-		$list = [];
-
-		foreach (array_reverse($values) as $index => $value) {
-			$list[] = new LinkedList(value: $value, next: $list[$index - 1] ?? null);
-		}
-
-		return end($list);
 	}
 }
 
-$head = LinkedListBuilder::build(['Foo', 'Bar', 'Baz', 'Qux']);
-assert($head->value === 'Foo');
-assert($head->next->value === 'Bar');
-assert($head->next->next->value === 'Baz');
-assert($head->next->next->next->value === 'Qux');
+$links = new LinkedList(
+	value: 'Foo', 
+	next: new LinkedList(
+		value: 'Bar',
+		next: new LinkedList(
+			value: 'Baz',
+			next: new LinkedList(value: 'Qux'),
+		),
+	),
+);
+
+assert($links->value === 'Foo');
+assert($links->next->value === 'Bar');
+assert($links->next->next->value === 'Baz');
+assert($links->next->next->next->value === 'Qux');
