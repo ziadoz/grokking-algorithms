a9f37930b2d8186dfb2bd9556629d31e28ed95c6
Jamie York <ziadoz@gmail.com>
2024-04-19 16:48:06 +0100

    recursive factorial

---
+++ ch3/factorial.php
@@ -0,0 +1,9 @@
+<?php
+// Factorial using recursion.
+// 5! = 5 * 4 * 3 * 2 * 1 = 120
+function factorial(int $x): int
+{
+	return $x === 1 ? 1 : $x * factorial($x - 1);
+}
+
+assert(factorial(5) === 120);
