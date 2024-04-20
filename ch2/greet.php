53775eb37c23b724a9d7a518ff9d7ad28e80ae25
Jamie York <ziadoz@gmail.com>
2024-04-20 18:29:21 +0100

    greet

---
+++ ch2/greet.php
@@ -0,0 +1,29 @@
+<?php
+function greet(string $name): void
+{
+    echo sprintf('hello, %s!' . "\n", $name);
+    greet2($name);
+    echo 'getting ready to say bye...' . "\n";
+    bye();
+}
+
+function greet2(string $name): void
+{
+    echo sprintf('how are you, %s?' . "\n", $name);
+}
+
+function bye(): void
+{
+    echo 'ok bye!' . "\n";
+}
+
+ob_start();
+greet('ziadoz');
+$output = trim(ob_get_clean());
+
+assert($output === <<<OUTPUT
+hello, ziadoz!
+how are you, ziadoz?
+getting ready to say bye...
+ok bye!
+OUTPUT);
