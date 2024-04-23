d9c28b1137f136464fef6d10ae9104f762181605
Jamie York <ziadoz@gmail.com>
2024-04-23 18:21:35 +0100

    items

--- ch4/02_recursive_sum.php
+++ ch4/02_recursive_sum.php
@@ -1,12 +1,12 @@
 <?php
 // @link https://github.com/egonSchiele/grokking_algorithms/blob/master/04_quicksort/php/02_recursive_count.php
-function recursive_sum(array $numbers): int
+function recursive_sum(array $items): int
 {
-    if (count($numbers) === 0) {
+    if (count($items) === 0) {
         return 0;
     }
 
-    return $numbers[0] + recursive_sum(array_slice($numbers, 1));
+    return $items[0] + recursive_sum(array_slice($items, 1));
 }
 
 assert(recursive_sum([1, 2, 3, 4]) === 10);
