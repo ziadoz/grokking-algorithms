779a88afbec56d0e9e9df8a9969e263c1a56d378
Jamie York <ziadoz@gmail.com>
2024-04-23 18:14:08 +0100

    use $items

--- ch4/01_loop_sum.php
+++ ch4/01_loop_sum.php
@@ -1,11 +1,11 @@
 <?php
 // @link https://github.com/egonSchiele/grokking_algorithms/blob/master/04_quicksort/php/01_loop_sum.php
-function loop_sum(array $numbers): int
+function loop_sum(array $items): int
 {
     $total = 0;
 
-    foreach ($numbers as $number) {
-        $total += $number;
+    foreach ($items as $item) {
+        $total += $item;
     }
 
     return $total;
