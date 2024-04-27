1a3d17fb7cf3c2060e243db7d27b277c79487cdf
Jamie York <ziadoz@gmail.com>
2024-04-27 14:41:21 +0100

    middle pivot quicksort

--- ch4/05_quicksort.php
+++ ch4/05_quicksort.php
@@ -1,5 +1,6 @@
 <?php
-function quicksort(array $items): array
+// Using the first item in the array as the pivot.
+function quicksort(array $items, ): array
 {
     if (count($items) < 2) {
         return $items;
@@ -16,3 +17,22 @@ function quicksort(array $items): array
 
 assert(quicksort([10, 5, 2, 3]) === [2, 3, 5, 10]);
 assert(quicksort([10, 100, 30, 96, 10_000, 42, 24, 12]) === [10, 12, 24, 30, 42, 96, 100, 10_000]);
+
+// Using the middle item in the array as the pivot.
+function quicksort_middle(array $items): array
+{
+    if (count($items) < 2) {
+        return $items;
+    }
+
+    $pivot = array_splice($items, floor(count($items) / 2), 1)[0];
+    $left  = array_filter($items, fn (int $item) => $item <= $pivot);
+    $right = array_filter($items, fn (int $item) => $item > $pivot);
+
+    echo '[' . implode(',', $left) . '] <= ' . $pivot . ' => [' . implode(',', $right) . ']' . "\n\n";
+
+    return [...quicksort_middle($left), $pivot, ...quicksort_middle($right)];
+}
+
+assert(quicksort_middle([10, 5, 2, 3]) === [2, 3, 5, 10]);
+assert(quicksort_middle([10, 100, 30, 96, 10_000, 42, 24, 12]) === [10, 12, 24, 30, 42, 96, 100, 10_000]);
