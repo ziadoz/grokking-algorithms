50aeb80451d891fec6424bdf3f2a659fca8ab370
Jamie York <ziadoz@gmail.com>
2024-04-27 15:59:13 +0100

    webpage cache

--- ch5/dicts.py
+++ ch5/dicts.py
@@ -33,3 +33,18 @@ def check_voter(name):
 check_voter("tom")
 check_voter("mike")
 check_voter("mike")
+
+# Webpage caching
+cache = {}
+
+def get_page(url):
+    if url in cache:
+        return cache[url]
+    else:
+        data = '<p>I am a webpage</p>'
+        cache[url] = data
+        return data
+
+print(get_page('facebook.com/login')) # Fresh...
+print(get_page('facebook.com/login')) # Cache...
+print(get_page('facebook.com/contact')) # Fresh...
